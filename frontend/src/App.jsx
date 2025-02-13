6ee1aadafe88ceeac3509d551026257eb8a0e1b9
Max <maxramm@y7mail.com>
2025-02-13 16:29:50 +1100

    [INIT] - initialise react frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import StockPage from './components/StockPage'
+
+const App = () => {
+  return (
+    <div>
+      <div className='container bg-red-500'>
+        <h1>Hello Test</h1>
+      </div>
+      <StockPage />
+    </div>
+  )
+}
+
+export default App
