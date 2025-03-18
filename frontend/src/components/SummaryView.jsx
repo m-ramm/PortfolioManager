f64d6bd71f37261175f10ee856d675ddb71f00e0
Max <maxramm@y7mail.com>
2025-03-18 20:29:16 +1100

    [FIX] - Skeleton pages fitting into website theming

---
+++ frontend/src/components/SummaryView.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+/*
+TODO:
+  - Total portfolio ticker
+  - Portfolio returns variance / mean
+  - recent trends
+  - 
+*/
+const SummaryView = () => {
+  return (
+    <div className='flex flex-col grow h-[calc(100vh-1.5rem)] bg-darker rounded-lg shadow-lg mt-4 mx-2 pt-4 px-2 mb-4'>
+      <div className='text-white text-lg font-bold'>Summary View</div>
+      <div className='text-white font-bold flex grow items-center justify-center'>Summary View currently under construction...</div>
+    </div>
+  )
+}
+
+export default SummaryView
--- frontend/src/pages/AboutPage.jsx
+++ frontend/src/pages/AboutPage.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
+    <div className='flex flex-col grow h-[calc(100vh-1.5rem)] bg-darker rounded-lg shadow-lg mt-4 mx-2 pt-4 px-2 mb-4'>
+      <div className='text-white text-lg font-bold ms-2 mt-2'>About</div>
+      <div className='text-white font-bold flex grow items-center justify-center'></div>
+    </div>
   )
 }
 
